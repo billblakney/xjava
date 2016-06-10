a1080738b79b930f9ea929746a7d2a10f2881939
billblakney <b.blakney@cox.net>
2016-06-10 14:59:19 -0400

    add XDialogs

---
+++ src/Dialog.java
@@ -0,0 +1,141 @@
+import javax.swing.JDialog; 
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionListener;
+import java.util.Vector;
+import javax.swing.JComboBox;
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFileChooser;
+import javax.swing.JTextField;
+import java.awt.event.ActionEvent;
+import java.io.File;
+
+public class Dialog extends JDialog implements ActionListener
+{
+    private JPanel _panel = null;
+
+    /** Ok and Cancel buttons. */
+    private JButton _okButton = null;
+    private JButton _cancelButton = null;
+
+    private JTextField _filenameField = null;
+    private JButton _chooseFileButton = null;
+
+    private JComboBox<String> _someComboBox = null;
+    private boolean _pressedOk = false;
+    private String _fileDir = null;
+
+    public Dialog(JFrame aOwner)
+    {
+       super(aOwner, true/*modal*/);
+
+       _fileDir = ".";
+
+       _panel = new JPanel();
+       getContentPane().add(_panel);
+
+       String[] tSomeNames = {"John", "Mary", "Sylvester" };
+       _someComboBox = new JComboBox<String>(tSomeNames);
+       _panel.add(_someComboBox);
+
+       _filenameField = new JTextField(40);
+       _panel.add(_filenameField);
+
+       _chooseFileButton = new JButton("File...");
+       _chooseFileButton.addActionListener(this);
+       _panel.add(_chooseFileButton); 
+
+       _okButton = new JButton("OK");
+       _okButton.addActionListener(this);
+       _panel.add(_okButton); 
+
+       _cancelButton = new JButton("Cancel");
+       _cancelButton.addActionListener(this);
+       _panel.add(_cancelButton);  
+
+       getContentPane().setLayout(new BorderLayout());
+
+       JPanel centerPnl = new JPanel();
+       centerPnl.setLayout(new BorderLayout());
+       JPanel gridLayoutPane = new JPanel(); 
+       GridLayout gridLayout = new GridLayout(2,3);
+
+       gridLayoutPane.setLayout(gridLayout);
+       gridLayoutPane.add(new JLabel("Group:"));
+       gridLayoutPane.add(_someComboBox);
+       gridLayoutPane.add(new JLabel("Empty"));
+       gridLayoutPane.add(new JLabel("File:"));
+       gridLayoutPane.add(_filenameField);
+       gridLayoutPane.add(_chooseFileButton);
+
+       centerPnl.add(gridLayoutPane, BorderLayout.CENTER);
+       //      centerPnl.add(new JLabel("Long label should be going here"), BorderLayout.SOUTH);
+
+       add(centerPnl, BorderLayout.CENTER);
+
+       JPanel bottomPanel = new JPanel();
+       bottomPanel.add(_okButton);
+       bottomPanel.add(_cancelButton);
+
+       add(bottomPanel, BorderLayout.SOUTH);
+
+
+       pack();
+       setLocationRelativeTo(aOwner);
+       setSize(200,150);
+       setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        if(_okButton == e.getSource()) {
+            System.err.println("User chose OK.");
+            _pressedOk = true;
+            setVisible(false);
+        }
+        else if(_cancelButton == e.getSource()) {
+            System.err.println("User chose Cancel.");
+            _pressedOk = false;
+            setVisible(false);
+        }
+        else if(_chooseFileButton == e.getSource()) {
+            System.err.println("User pressed file button.");
+            chooseFile();
+            _pressedOk = false;
+        }
+    }
+    
+    private void chooseFile()
+    {
+
+    	JFileChooser chooser = new JFileChooser();
+    	chooser.setCurrentDirectory(new File(_fileDir));
+    	int tOption = chooser.showOpenDialog(this);
+    	if (tOption == JFileChooser.APPROVE_OPTION) {
+    		String tFile = chooser.getSelectedFile().getPath();
+    		System.out.println("File selected: " + tFile);
+    		// load the trades
+    		_filenameField.setText(tFile);
+    	} else {
+    		System.out.println("File selection canceled");
+    	}
+    }
+
+    public boolean pressedOk()
+    {
+    	return _pressedOk;
+    }
+    
+    public String getAccountName()
+    {
+    	return (String) _someComboBox.getSelectedItem();
+    }
+    
+    public String getTradeFileName()
+    {
+    	return _filenameField.getText();
+    }
+}
---
+++ src/XDialogs.java
@@ -0,0 +1,96 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import java.awt.Color;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+
+public class XDialogs implements ActionListener
+{
+   private JFrame frame;
+
+   /**
+    * Launch the application.
+    */
+   public static void main(String[] args)
+   {
+      EventQueue.invokeLater(new Runnable()
+      {
+         public void run()
+         {
+            System.out.println("running XDialogs");
+            try
+            {
+               XDialogs window = new XDialogs();
+               window.frame.setVisible(true);
+            }
+            catch (Exception e)
+            {
+               e.printStackTrace();
+            }
+         }
+      });
+   }
+
+   /**
+    * Create the application.
+    */
+   public XDialogs()
+   {
+      initialize();
+   }
+
+   /**
+    * Initialize the contents of the frame.
+    */
+   private void initialize()
+   {
+      frame = new JFrame();
+//      frame.getContentPane().setBackground(new Color(0, 0, 128));
+      frame.setBounds(100, 100, 868, 865);
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.getContentPane().setLayout(null);
+
+      JMenuBar tMenuBar = new JMenuBar();
+      tMenuBar.setBounds(0, 0, 200, 21);
+      frame.getContentPane().add(tMenuBar);
+
+      /* file menu */
+      JMenu tFileMenu = new JMenu("File Menu");
+      tMenuBar.add(tFileMenu);
+
+      JMenuItem tSayHelloMenuItem = new JMenuItem("Say \"Hello\"");
+      tSayHelloMenuItem.addActionListener(this);
+      tFileMenu.add(tSayHelloMenuItem);
+
+      /* edit menu */
+      JMenu tEditMenu = new JMenu("Edit Menu");
+      tMenuBar.add(tEditMenu);
+
+      JMenuItem tEditMenuItem = new JMenuItem("Edit");
+      tEditMenu.add(tEditMenuItem);
+
+      /* test menu */
+      JMenu tTestMenu = new JMenu("Test Menu");
+      tMenuBar.add(tTestMenu);
+
+      JMenuItem tTestMenuItem = new JMenuItem("Test");
+      tTestMenu.add(tTestMenuItem);
+   }
+		public void actionPerformed(ActionEvent e) {
+			// Handle "File Menu".
+			if (e.getActionCommand().equals("Say \"Hello\"")) {
+				System.out.println("User selected File Menu");
+				Dialog tDialog = new Dialog(frame);
+				if (tDialog.pressedOk()){
+				   System.out.println("User pressed OK");
+				}
+			}
+		}
+}
