373c82db9d8c1e3cca5f9bc04de7a18c444c7b9d
billblakney <spam.blakney@cox.net>
2016-06-14 07:22:36 -0400

    refactor - mainly extract methods for more clarity

--- src/DialogBoxGridBagLayout.java
+++ src/DialogBoxGridBagLayout.java
@@ -1,4 +1,4 @@
-import javax.swing.JDialog; 
+import javax.swing.JDialog;
 import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.Dimension;
@@ -20,215 +20,274 @@ import java.io.File;
 
 public class DialogBoxGridBagLayout extends JDialog implements ActionListener
 {
-    private JPanel _panel = null;
+   private JPanel _panel = null;
 
-    /** Ok and Cancel buttons. */
-    private JButton _buttonOk = null;
-    private JButton _buttonCancel = null;
+   // Ok and Cancel buttons
+   private JButton _okButton = null;
+   private JButton _cancelButton = null;
 
-    private JTextField _textFieldFile = null;
-    private JButton _buttonBrowse = null;
-    
-    private JLabel _labelName = null;
-    private JLabel _labelFile = null;
+   // Row 1: label, combobox
+   private JLabel _nameLabel = null;
+   private JComboBox<String> _nameComboBox = null;
 
-    private JComboBox<String> _comboBoxName = null;
-    private boolean _pressedOk = false;
-    private String _fileDir = null;
+   // Row 2: label, file text and browse button
+   private JLabel _fileLabel = null;
+   private JTextField _fileTextField = null;
+   private JButton _fileBrowseButton = null;
+   private JPanel _filePane = null;
 
-    public DialogBoxGridBagLayout(JFrame aOwner)
-    {
-       super(aOwner, true/*modal*/);
-       
-       Dimension tDim;
+   // selected file
+   private String _fileDir = null;
 
-       _fileDir = ".";
+   // pressed ok or cancel
+   private boolean _pressedOk = false;
 
-       _panel = new JPanel();
-       getContentPane().add(_panel);
+   /**
+    * Use GridBagLayout for dialog user choices
+    */
+   public DialogBoxGridBagLayout(JFrame aOwner)
+   {
+      super(aOwner, true/* modal */);
 
-       /*
-        * Create the components.
-        */
-       String[] tSomeNames = {"John", "Mary", "Sylvester" };
-       _comboBoxName = new JComboBox<String>(tSomeNames);
-       _panel.add(_comboBoxName);
+      Dimension tDim;
 
-       /*
-        * Create text field for filename.
-        */
-       _textFieldFile = new JTextField(40);
-       tDim = _textFieldFile.getPreferredSize();
-       tDim.width = 200;
-       _textFieldFile.setPreferredSize(tDim);
-       _panel.add(_textFieldFile);
+      _fileDir = ".";
 
-       /*
-        * Create the browse button.
-        */
-       _buttonBrowse = new JButton("Browse...");
-       _buttonBrowse.addActionListener(this);
-       tDim = _textFieldFile.getPreferredSize();
-       tDim.width += 60;
-       _textFieldFile.setPreferredSize(tDim);
-       _panel.add(_buttonBrowse); 
-       
-       /*
-        * Create the Name and File labels.
-        */
-       _labelName = new JLabel("Name:");
-       _labelFile = new JLabel("File:");
+      _panel = new JPanel();
+      getContentPane().add(_panel);
 
-       /*
-        * Create the Ok and Cancel buttons.
-        */
-       _buttonOk = new JButton("OK");
-       _buttonOk.addActionListener(this);
-       _panel.add(_buttonOk); 
+      // create _nameLabel and _nameComboBox
+      createComponentsRow1();
 
-       _buttonCancel = new JButton("Cancel");
-       _buttonCancel.addActionListener(this);
-       _panel.add(_buttonCancel);  
-       
-       _buttonOk.setPreferredSize(_buttonCancel.getPreferredSize());
-       
-       /*
-        * Use a BoxLayout pane for the filename field and browse button.
-        */
-       JPanel tFilePane = new JPanel();
-       tFilePane.setLayout(new BoxLayout(tFilePane,BoxLayout.LINE_AXIS));
-       tFilePane.add(_textFieldFile);
-       tFilePane.add(Box.createRigidArea(new Dimension(5,0)));
-       tFilePane.add(_buttonBrowse);
-       
-       /*
-        * Use a GridBagLayout for the upper settings area.
-        * How to use GridBagLayout:
-        * https://docs.oracle.com/javase/tutorial/uiswing/layout/gridbag.html
-        */
-       JPanel tGridPane = new JPanel(new GridBagLayout());
-       GridBagConstraints c = new GridBagConstraints();
+      // create _fileLable, _fileTextField, _fileBrowseButton
+      createComponentsRow2();
 
-       // common settings for column 1
-       c.fill = GridBagConstraints.NONE; // use natural size
-       c.anchor = GridBagConstraints.LINE_END; // right align
+      // create the main pane for user inputs
+      JPanel tGridPane = createGridPane();
 
-       // buffer on top of first row
-       int tTopEdgeSpace = 20;
-       // buffer at bottom top of last row
-       int tBottomEdgeSpace = 10;
-       // buffer at right edge of last column
-       int tRightEdgeSpace = 20;
-       // buffer at left edge of first column
-       int tLeftEdgeSpace = 10;
-       
-       // settings for name label
-       c.gridx = 0;
-       c.gridy = 0;
-       c.insets = new Insets(tTopEdgeSpace,tLeftEdgeSpace,5,5);
-       tGridPane.add(_labelName, c);
+      // create the ok/cancel button pane
+      JPanel tButtonPane = createButtonPane();
 
-       // settings for file label
-       c.gridx = 0;
-       c.gridy = 1;
-       c.insets = new Insets(5,tLeftEdgeSpace,tBottomEdgeSpace,5);
-       tGridPane.add(_labelFile, c);
+      // setup the main content pane
+      Container contentPane = getContentPane();
+      contentPane.add(tGridPane, BorderLayout.CENTER);
+      contentPane.add(tButtonPane, BorderLayout.PAGE_END);
 
-       // common settings for column w
-       c.anchor = GridBagConstraints.LINE_START; // left align
+      pack();
+      setLocationRelativeTo(aOwner);
+      setSize(400, 160);
+      setVisible(true);
+   }
 
-       // settings for name combobox
-       c.gridx = 1;
-       c.gridy = 0;
-       c.weightx = 0.25;
-       c.insets = new Insets(tTopEdgeSpace,5,5,tRightEdgeSpace);
-       tGridPane.add(_comboBoxName, c);
+   /**
+    */
+   JPanel createGridPane()
+   {
+      /*
+       * Use a GridBagLayout for the upper settings area. How to use
+       * GridBagLayout:
+       * https://docs.oracle.com/javase/tutorial/uiswing/layout/gridbag.html
+       */
+      JPanel tGridPane = new JPanel(new GridBagLayout());
+      GridBagConstraints c = new GridBagConstraints();
 
-       // settings for file area
-       c.fill = GridBagConstraints.HORIZONTAL; // stretch to fill horizontal
+      // common settings for column 1
+      c.fill = GridBagConstraints.NONE; // use natural size
+      c.anchor = GridBagConstraints.LINE_END; // right align
 
-       c.gridx = 1;
-       c.gridy = 1;
-       c.weightx = 0.95;
-       c.insets = new Insets(5,5,tBottomEdgeSpace,tRightEdgeSpace);
-       tGridPane.add(tFilePane, c);
+      _filePane = createFilePane();
 
-       /*
-        * Use a BoxLayout for the Ok/Cancel button panel.
-        * How to use BoxLayout:
-        * https://docs.oracle.com/javase/tutorial/uiswing/layout/box.html
-        */
-       JPanel tButtonPane = new JPanel();
-       tButtonPane.add(Box.createHorizontalGlue());
-       tButtonPane.setLayout(new BoxLayout(tButtonPane, BoxLayout.LINE_AXIS));
-       tButtonPane.add(_buttonOk);
-       tButtonPane.add(Box.createRigidArea(new Dimension(25,0)));
-       tButtonPane.add(_buttonCancel);
-       tButtonPane.add(Box.createHorizontalGlue());
+      // buffer on top of first row
+      int tTopEdgeSpace = 20;
+      // buffer at bottom top of last row
+      int tBottomEdgeSpace = 10;
+      // buffer at right edge of last column
+      int tRightEdgeSpace = 20;
+      // buffer at left edge of first column
+      int tLeftEdgeSpace = 10;
 
-       tDim = _buttonOk.getPreferredSize();
-       tDim.height += 20;
-       tButtonPane.add(Box.createRigidArea(tDim));
+      // settings for name label
+      c.gridx = 0;
+      c.gridy = 0;
+      c.insets = new Insets(tTopEdgeSpace, tLeftEdgeSpace, 5, 5);
+      tGridPane.add(_nameLabel, c);
 
-       /*
-        * Add the upper grid pane and Ok/Cancel buttons pane to the main pane.
-        */
-       Container contentPane = getContentPane();
-       contentPane.add(tGridPane, BorderLayout.CENTER);
-       contentPane.add(tButtonPane, BorderLayout.PAGE_END);
+      // settings for file label
+      c.gridx = 0;
+      c.gridy = 1;
+      c.insets = new Insets(5, tLeftEdgeSpace, tBottomEdgeSpace, 5);
+      tGridPane.add(_fileLabel, c);
 
-       pack();
-       setLocationRelativeTo(aOwner);
-       setSize(400,160);
-       setVisible(true);
-    }
+      // common settings for column w
+      c.anchor = GridBagConstraints.LINE_START; // left align
 
-    public void actionPerformed(ActionEvent e) {
-        if(_buttonOk == e.getSource()) {
-            System.err.println("User chose OK.");
-            _pressedOk = true;
-            setVisible(false);
-        }
-        else if(_buttonCancel == e.getSource()) {
-            System.err.println("User chose Cancel.");
-            _pressedOk = false;
-            setVisible(false);
-        }
-        else if(_buttonBrowse == e.getSource()) {
-            System.err.println("User pressed file button.");
-            chooseFile();
-            _pressedOk = false;
-        }
-    }
-    
-    private void chooseFile()
-    {
+      // settings for name combobox
+      c.gridx = 1;
+      c.gridy = 0;
+      c.weightx = 0.25;
+      c.insets = new Insets(tTopEdgeSpace, 5, 5, tRightEdgeSpace);
+      tGridPane.add(_nameComboBox, c);
 
-    	JFileChooser chooser = new JFileChooser();
-    	chooser.setCurrentDirectory(new File(_fileDir));
-    	int tOption = chooser.showOpenDialog(this);
-    	if (tOption == JFileChooser.APPROVE_OPTION) {
-    		String tFile = chooser.getSelectedFile().getPath();
-    		System.out.println("File selected: " + tFile);
-    		// load the trades
-    		_textFieldFile.setText(tFile);
-    	} else {
-    		System.out.println("File selection canceled");
-    	}
-    }
+      // settings for file area
+      c.fill = GridBagConstraints.HORIZONTAL; // stretch to fill horizontal
 
-    public boolean pressedOk()
-    {
-    	return _pressedOk;
-    }
-    
-    public String getAccountName()
-    {
-    	return (String) _comboBoxName.getSelectedItem();
-    }
-    
-    public String getTradeFileName()
-    {
-    	return _textFieldFile.getText();
-    }
+      c.gridx = 1;
+      c.gridy = 1;
+      c.weightx = 0.95;
+      c.insets = new Insets(5, 5, tBottomEdgeSpace, tRightEdgeSpace);
+      tGridPane.add(_filePane, c);
+
+      return tGridPane;
+   }
+
+   /**
+    * Creates row1: Name: <name_combobox>
+    */
+   void createComponentsRow1()
+   {
+      _nameLabel = new JLabel("Name:");
+
+      String[] tSomeNames = { "John", "Mary", "Sylvester" };
+      _nameComboBox = new JComboBox<String>(tSomeNames);
+   }
+
+   /**
+    * Creates row2: File: <text_box> <browse_button>
+    */
+   void createComponentsRow2()
+   {
+      _fileLabel = new JLabel("File:");
+
+      // Create text field for filename.
+      {
+      _fileTextField = new JTextField(40);
+      Dimension tDim = _fileTextField.getPreferredSize();
+      tDim.width = 200;
+      _fileTextField.setPreferredSize(tDim);
+      _panel.add(_fileTextField);
+      }
+
+      // create the browse button
+      {
+      _fileBrowseButton = new JButton("Browse...");
+      _fileBrowseButton.addActionListener(this);
+      Dimension tDim = _fileTextField.getPreferredSize();
+      tDim = _fileTextField.getPreferredSize();
+      tDim.width += 60;
+      _fileTextField.setPreferredSize(tDim);
+      _panel.add(_fileBrowseButton);
+      }
+   }
+
+   /**
+    * Create the panel to hold the file text field/browse button combination.
+    */
+   JPanel createFilePane()
+   {
+      /*
+       * Use a BoxLayout pane for the filename field and browse button.
+       */
+      JPanel tFilePane = new JPanel();
+      tFilePane.setLayout(new BoxLayout(tFilePane, BoxLayout.LINE_AXIS));
+      tFilePane.add(_fileTextField);
+      tFilePane.add(Box.createRigidArea(new Dimension(5, 0)));
+      tFilePane.add(_fileBrowseButton);
+
+      return tFilePane;
+   }
+
+   /**
+    * Create the ok and cancel button pane and its components.
+    */
+   JPanel createButtonPane()
+   {
+      // create the Ok button
+      _okButton = new JButton("OK");
+      _okButton.addActionListener(this);
+      _panel.add(_okButton);
+
+      // create the Cancel button
+      _cancelButton = new JButton("Cancel");
+      _cancelButton.addActionListener(this);
+      _panel.add(_cancelButton);
+
+      // make ok button size same as cancel size
+      _okButton.setPreferredSize(_cancelButton.getPreferredSize());
+
+      /*
+       * Use a BoxLayout for the Ok/Cancel button panel. How to use BoxLayout:
+       * https://docs.oracle.com/javase/tutorial/uiswing/layout/box.html
+       */
+      JPanel tButtonPane = new JPanel();
+      tButtonPane.add(Box.createHorizontalGlue());
+      tButtonPane.setLayout(new BoxLayout(tButtonPane, BoxLayout.LINE_AXIS));
+      tButtonPane.add(_okButton);
+      tButtonPane.add(Box.createRigidArea(new Dimension(25, 0)));
+      tButtonPane.add(_cancelButton);
+      tButtonPane.add(Box.createHorizontalGlue());
+
+      /*
+       * To enlarge the button pane, add a rigid area.
+       */
+      Dimension tDim = _okButton.getPreferredSize();
+      tDim.height += 20;
+      tButtonPane.add(Box.createRigidArea(tDim));
+      return tButtonPane;
+   }
+
+   public void actionPerformed(ActionEvent e)
+   {
+      if (_okButton == e.getSource())
+      {
+         System.err.println("User chose OK.");
+         _pressedOk = true;
+         setVisible(false);
+      }
+      else if (_cancelButton == e.getSource())
+      {
+         System.err.println("User chose Cancel.");
+         _pressedOk = false;
+         setVisible(false);
+      }
+      else if (_fileBrowseButton == e.getSource())
+      {
+         System.err.println("User pressed file button.");
+         chooseFile();
+         _pressedOk = false;
+      }
+   }
+
+   private void chooseFile()
+   {
+
+      JFileChooser chooser = new JFileChooser();
+      chooser.setCurrentDirectory(new File(_fileDir));
+      int tOption = chooser.showOpenDialog(this);
+      if (tOption == JFileChooser.APPROVE_OPTION)
+      {
+         String tFile = chooser.getSelectedFile().getPath();
+         System.out.println("File selected: " + tFile);
+         // load the trades
+         _fileTextField.setText(tFile);
+      }
+      else
+      {
+         System.out.println("File selection canceled");
+      }
+   }
+
+   public boolean pressedOk()
+   {
+      return _pressedOk;
+   }
+
+   public String getAccountName()
+   {
+      return (String) _nameComboBox.getSelectedItem();
+   }
+
+   public String getTradeFileName()
+   {
+      return _fileTextField.getText();
+   }
 }
