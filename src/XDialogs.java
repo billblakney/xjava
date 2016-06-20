2bdb4b6c22c67907e38441d48a33f6af72436b6f
billblakney <b.blakney@cox.net>
2016-06-20 18:32:24 -0400

    organize imports for XDialogs

--- src/XDialogs.java
+++ src/XDialogs.java
@@ -1,15 +1,10 @@
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
-import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JFrame;
-import javax.swing.JTextField;
-import java.awt.Color;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
-import javax.swing.JPanel;
 
 public class XDialogs implements ActionListener
 {
