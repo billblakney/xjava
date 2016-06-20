306925d8573f81ff2e0874ba1148b98c393d2efa
billblakney <b.blakney@cox.net>
2016-06-20 18:34:38 -0400

    organize imports and format IdComboBox

--- src/IdComboBox.java
+++ src/IdComboBox.java
@@ -1,54 +1,53 @@
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Vector;
-import javax.swing.JComboBox;
 import javafx.util.Pair;
-
+import javax.swing.JComboBox;
 
 /**
- * Combobox for choosing from a list of (Integer,String) pairs.
- * The Integer is referred to as the "ID", and the String as its "name".
- * The combobox displays the names. The getSelectedId method returns the
- * ID corresponding to the selected name.
+ * Combobox for choosing from a list of (Integer,String) pairs. The Integer is
+ * referred to as the "ID", and the String as its "name". The combobox displays
+ * the names. The getSelectedId method returns the ID corresponding to the
+ * selected name.
  */
 public class IdComboBox extends JComboBox<String>
 {
    /**
-    * Mapping of JComboBox selection indices to the IDs.
-    * The key is a selection index, and the value is the corresponding ID.
+    * Mapping of JComboBox selection indices to the IDs. The key is a selection
+    * index, and the value is the corresponding ID.
     */
-   protected Map<Integer,Integer> _idMap = new HashMap<Integer,Integer>();
-   
+   protected Map<Integer, Integer> _idMap = new HashMap<Integer, Integer>();
+
    /**
-    * Constructs an IdComboBox from a map of (ID,name) entries.
-    * To ensure the desired order of names in the combobox, the user should
-    * use the appropriate type of Map, e.g. TreeMap to order the selection
-    * list by the natural ordering of the map keys, or LinkedHashMap to
-    * order the selection list per the order items were added to the map.
+    * Constructs an IdComboBox from a map of (ID,name) entries. To ensure the
+    * desired order of names in the combobox, the user should use the
+    * appropriate type of Map, e.g. TreeMap to order the selection list by the
+    * natural ordering of the map keys, or LinkedHashMap to order the selection
+    * list per the order items were added to the map.
     */
-   public IdComboBox(Map<Integer,String> aIds)
+   public IdComboBox(Map<Integer, String> aIds)
    {
       int i = 0;
-      for (Integer tId: aIds.keySet())
+      for (Integer tId : aIds.keySet())
       {
          addItem(aIds.get(tId));
-         _idMap.put(i++,tId);
+         _idMap.put(i++, tId);
       }
    }
-   
+
    /**
     * Constructs an IdComboBox from a vector of (ID,name) pairs.
     */
-   public IdComboBox(Vector<Pair<Integer,String>> aIds)
+   public IdComboBox(Vector<Pair<Integer, String>> aIds)
    {
       int i = 0;
-      for (Pair<Integer,String> tIdPair: aIds)
+      for (Pair<Integer, String> tIdPair : aIds)
       {
          addItem(tIdPair.getValue());
-         _idMap.put(i++,tIdPair.getKey());
+         _idMap.put(i++, tIdPair.getKey());
       }
    }
-   
+
    /**
     * Get the ID of the selected JComboBox item.
     */
