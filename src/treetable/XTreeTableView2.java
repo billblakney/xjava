15f9eb9a553ea93f14f35b1ab23f5e0bdb16c924
billblakney <b.blakney@cox.net>
2016-07-14 16:26:27 -0400

    some cleanup

--- src/treetable/XTreeTableView2.java
+++ src/treetable/XTreeTableView2.java
@@ -19,24 +19,17 @@ public class XTreeTableView2 extends Application {
   }
   @Override
   public void start(Stage stage) {
-    final Scene scene = new Scene(new Group(), 200, 400);
+    final Scene scene = new Scene(new Group(), 400, 300);
     Group sceneRoot = (Group) scene.getRoot();
     
     TreeItem<TableEntry> tChild1 = new TreeItem<>(new TableEntry(1,"Joe",10.0));
     TreeItem<TableEntry> tChild2 = new TreeItem<>(new TableEntry(2,"Mary",99.0));
     TreeItem<TableEntry> tChild3 = new TreeItem<>(new TableEntry(3,"Harris",45.3));
 
-//    TreeItem<String> childNode1 = new TreeItem<>("Node 1");
-//    TreeItem<String> childNode2 = new TreeItem<>("Node 2");
-//    TreeItem<String> childNode3 = new TreeItem<>("Node 3");
-
     TreeItem<TableEntry> root = new TreeItem<>(new TableEntry());
     root.setExpanded(true);
 
     root.getChildren().setAll(tChild1, tChild2, tChild3);
-
-    TreeTableColumn<TableEntry, Integer> column =
-          new TreeTableColumn<>("Id");
 
     TreeTableColumn<TableEntry,Integer> idColumn = new TreeTableColumn<>("Id");
     idColumn.setPrefWidth(150);
@@ -53,7 +46,7 @@ public class XTreeTableView2 extends Application {
        }
      );
 
-    TreeTableColumn<TableEntry,String> nameColumn = new TreeTableColumn<>("Id");
+    TreeTableColumn<TableEntry,String> nameColumn = new TreeTableColumn<>("Name");
     nameColumn.setPrefWidth(150);
     nameColumn.setCellValueFactory(
        new Callback<CellDataFeatures<TableEntry,String>,ObservableValue<String>>()
@@ -66,33 +59,28 @@ public class XTreeTableView2 extends Application {
        }
      );
 
-    TreeTableColumn<TableEntry,Double> valueColumn = new TreeTableColumn<>("Id");
+    // short way
+    TreeTableColumn<TableEntry,Double> valueColumn = new TreeTableColumn<>("Value");
     valueColumn.setPrefWidth(150);
-    valueColumn.setCellValueFactory(
-       new Callback<CellDataFeatures<TableEntry,Double>,ObservableValue<Double>>()
-       {
-          @Override public ObservableValue<Double>
-          call(CellDataFeatures<TableEntry,Double> p)
-          {
-             // Note: SimpleIntegerProperty implements ObservableValue<Number>,
-             // so must use this workaround.
-             return p.getValue().getValue().valueProperty().asObject();
-          }
-       }
-     );
+    valueColumn.setCellValueFactory((p)-> 
+          p.getValue().getValue().valueProperty().asObject()
+       );
 
-//    column.setCellValueFactory((CellDataFeatures<String, String> p) -> new ReadOnlyStringWrapper(
-//            p.getValue().getValue()));
-    
-    //first getValue() Returns the value passed in to the constructor
-    //getValue() Returns the data represented by this TreeItem
-//    column.setCellValueFactory((CellDataFeatures p) -> new ReadOnlyStringWrapper(p.getValue().getValue())); 
-    
-//    column.setCellValueFactory(new Callback<CellDataFeatures<String, String>, ObservableValue<String>>() {
-//       @Override public ObservableValue<String> call(CellDataFeatures<String, String> p) {
-//           return new ReadOnlyStringWrapper(p.getValue().getValue());
+    // long way
+//    TreeTableColumn<TableEntry,Double> valueColumn = new TreeTableColumn<>("Value");
+//    valueColumn.setPrefWidth(150);
+//    valueColumn.setCellValueFactory(
+//       new Callback<CellDataFeatures<TableEntry,Double>,ObservableValue<Double>>()
+//       {
+//          @Override public ObservableValue<Double>
+//          call(CellDataFeatures<TableEntry,Double> p)
+//          {
+//             // ?Note: SimpleDoubleProperty implements ObservableValue<Number>,
+//             // so must use this workaround.
+//             return p.getValue().getValue().valueProperty().asObject();
+//          }
 //       }
-//   });
+//     );
 
     TreeTableView<TableEntry> treeTableView = new TreeTableView<>(root);
     treeTableView.getColumns().add(idColumn);
